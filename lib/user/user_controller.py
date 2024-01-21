74ea6c426d0e3636adef1059c62ea527472cfa23
benhurst1 <ben-hurst@outlook.com>
2024-01-21 12:35:41 +0000

    password and username min requirements

--- lib/user/user_controller.py
+++ lib/user/user_controller.py
@@ -1,19 +1,21 @@
 from lib.user.user import User
-import bcrypt
+import bcrypt, re
 
 
 class UserController:
     def __init__(self, connection):
         self._connection = connection
+        self.deny = """ ?<>!@#$%^&*()'[]\{\}\\`~+=|"""
 
     def add_user(self, username, password, email):
-        hashed = self._hash_password(password)
-        rows = self._connection.execute(
-            """INSERT INTO users (username, email, password) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING RETURNING id """,
-            [username, email, hashed],
-        )
-        if len(rows) == 1:
-            return True
+        if self._check_username(username) and self._check_password_min_req(password):
+            hashed = self._hash_password(password)
+            rows = self._connection.execute(
+                """INSERT INTO users (username, email, password) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING RETURNING id """,
+                [username, email, hashed],
+            )
+            if len(rows) == 1:
+                return True
         return False
 
     def auth_user(self, username, password):
@@ -28,7 +30,9 @@ class UserController:
         return None
 
     def change_password(self, username, password, new_password):
-        if self._check_password(username, password):
+        if self._check_password(username, password) and self._check_password_min_req(
+            new_password
+        ):
             new_hashed = self._hash_password(new_password)
             self._connection.execute(
                 """UPDATE users SET password = %s WHERE username = %s""",
@@ -39,6 +43,19 @@ class UserController:
         self._connection.execute(
             """DELETE FROM users WHERE username = %s""", [username]
         )
+
+    def _check_username(self, username):
+        if re.match(r"^[A-Za-z0-9_]+$", username):
+            return True
+        return False
+
+    def _check_password_min_req(self, input_password):
+        if re.match(
+            r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$",
+            input_password,
+        ):
+            return True
+        return False
 
     def _hash_password(self, password):
         salt = bcrypt.gensalt(14)
