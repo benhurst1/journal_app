c210f834b1d265ebf6ee5605f723f705fd59f085
benhurst1 <ben-hurst@outlook.com>
2024-01-23 17:29:51 +0000

    database string and workflow adjust

--- lib/db_connection.py
+++ lib/db_connection.py
@@ -5,16 +5,16 @@ class DatabaseConnection:
     DEV_DATABASE_NAME = "journal_app"
     TEST_DATABASE_NAME = "journal_app_test"
 
-
     def __init__(self, test_mode=False):
         self.test_mode = test_mode
         self.connect()
 
     def connect(self):
         try:
-            self.connection = psycopg2.connect(
-                f"postgresql://localhost/{self._database_name()}"
-            )
+            # self.connection = psycopg2.connect(
+            #     f"postgresql://localhost/{self._database_name()}"
+            # )
+            self.connection = psycopg2.connect(f"dbname={self._database_name()}")
         except psycopg2.OperationalError:
             raise Exception(f"Could not connect to {self._database_name()}")
 
